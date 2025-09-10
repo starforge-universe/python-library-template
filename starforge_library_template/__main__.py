d8984a1bbd62a4c9727ed13484a3270964a38b58
Star Forge Worker <star.forge.worker@gmail.com>
2025-09-10 21:57:10 +0200

    template elements update

---
+++ starforge_library_template/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Main entry point for Starforge Library template.
+"""
+
+def main():
+    """
+    Main function to run the Starforge Library template.
+    """
+    print("Welcome to the Starforge Library template!")
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
